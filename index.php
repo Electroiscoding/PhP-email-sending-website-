25f20fa666dacfbc4c5f0662a89d127a8b5fa610
Soham Pal <103299713+Electroiscoding@users.noreply.github.com>
2024-04-27 12:31:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Send Email</title>
+</head>
+<body>
+
+<h2>Send Email</h2>
+<form action="send_email.php" method="post">
+    <input type="email" name="email" placeholder="Enter your email">
+    <button type="submit" name="submit">Send Email</button>
+</form>
+
+</body>
+</html
